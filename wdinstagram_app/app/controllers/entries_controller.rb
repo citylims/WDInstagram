a90909a16cf4a00b670a8b381a70610c05a0d54f
Ausin Formica <austin.formica@gmail.com>
2015-01-10 15:17:58 -0500

    add actions to entries controller

--- wdinstagram_app/app/controllers/entries_controller.rb
+++ wdinstagram_app/app/controllers/entries_controller.rb
@@ -1,3 +1,54 @@
 class EntriesController < ApplicationController
+	before_action :set_entry, only: [:show, :edit, :update, :destroy]
+
+	def index
+  	@entries = Entry.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def new
+  	@entries = Entry.new
+  end
+
+  def create
+  	@entry = Entry.build(joke_params)
+  	if @entry.save
+  		redirect_to entry_path(@entry)
+  	else
+  		render :new
+  	end
+  	
+  end
+
+  def update
+  	@entry.update(entry_params)
+  	if @entry.save
+  		redirect_to entry_path(@entry)
+  	else
+  		render :edit
+  	end
+  end
+
+  def destroy
+  	@entry.destroy
+  	redirect_to entries_path 
+  end
+
+  private
+
+  def entry_params
+  	params.require(:entry).permit(:author, :photo_url, :date_taken, :caption)
+  end
+
+  def set_entry
+  	@entry = Entry.find(params[:id])
+  end
 
 end
