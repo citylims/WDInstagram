1d4e5f5569f83a65b5ab2ea7da85ae4dda899afe
Ausin Formica <austin.formica@gmail.com>
2015-01-10 15:42:27 -0500

    create seed file

--- wdinstagram_app/db/seeds.rb
+++ wdinstagram_app/db/seeds.rb
@@ -1,6 +1,6 @@
 Entry.destroy_all
 
-entry = Entry.create(author: "Bob!", photo_url:"http://i.imgur.com/eEXkhQ0.jpg", caption:"Tee Hee Ha Ha Haw Woop Ha Hi Hi Ho", date_taken: "1/10/15")
+entry = Entry.create(author: "Bob!", photo_url:"http://i.imgur.com/ttiHmtH.jpg", caption:"Tee Hee Ha Ha Haw Woop Ha Hi Hi Ho", date_taken: "1/10/15")
 
 
 
